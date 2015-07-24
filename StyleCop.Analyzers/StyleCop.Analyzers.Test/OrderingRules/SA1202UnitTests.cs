f10af6e25b5eecfd26cd00e750efe7bb4e604bac
Matt Bird <matthew.bird@icarehealth.co.uk>
2015-07-24 16:47:34 +0100

    SA1202 diagnostic implementation for fields, methods, delegates and events.

---
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test/OrderingRules/SA1202UnitTests.cs
@@ -0,0 +1,192 @@
+﻿namespace StyleCop.Analyzers.Test.OrderingRules
+{
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using Microsoft.CodeAnalysis.Diagnostics;
+
+    using StyleCop.Analyzers.OrderingRules;
+
+    using TestHelper;
+
+    using Xunit;
+    using Xunit.Extensions;
+
+    public class SA1202UnitTests : CodeFixVerifier
+    {
+        [Fact]
+        public async Task TestEmptySourceAsync()
+        {
+            var testCode = string.Empty;
+            await this.VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        [Fact]
+        public async Task TestFieldOrderCorrectOrderAsync()
+        {
+            string testCode = @"class Foo
+{ 
+    public int field1;
+    internal int field2;
+    protected internal int field3;
+    protected int field4;
+    private int field5;
+}";
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Enumerates combinations of modifiers for adajacent members that should report a diagnostic.
+        /// </summary>
+        public static IEnumerable<object[]> ModifierCombinations => new[]
+        {
+            new object[] { null, "protected" },
+            new object[] { null, "protected internal" },
+            new object[] { null, "internal" },
+            new object[] { null, "public" },
+            new object[] { "private", "protected" },
+            new object[] { "private", "protected internal" },
+            new object[] { "private", "internal" },
+            new object[] { "private", "public" },
+            new object[] { "protected", "protected internal" },
+            new object[] { "protected", "internal" },
+            new object[] { "protected", "public" },
+            new object[] { "protected internal", "internal" },
+            new object[] { "protected internal", "public" },
+            new object[] { "internal", "public" }
+        };
+
+        /// <summary>
+        /// Asserts that incorrect ordering for fields is reported correctly.
+        /// </summary>
+        /// <param name="modifier1">The access modifier for the first field.</param>
+        /// <param name="modifier2">The access modifier for the second field.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Theory]
+        [MemberData(nameof(ModifierCombinations))]
+        public async Task TestFieldOrderIncorrectReportsAsync(string modifier1, string modifier2)
+        {
+            string placeholder1 = FormatModifier(modifier1);
+            string placeholder2 = FormatModifier(modifier2);
+
+            string testCode =
+$@"class Foo
+{{ 
+    {placeholder1}int field1;
+    {placeholder2}int field2;
+}}";
+
+            var expected = this.CSharpDiagnostic().WithLocation(3, 5).WithArguments(GetModifierName(modifier1), modifier2, "fields");
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Asserts that incorrect ordering for methods is reported correctly.
+        /// </summary>
+        /// <param name="modifier1">The access modifier for the first method.</param>
+        /// <param name="modifier2">The access modifier for the second method.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Theory]
+        [MemberData(nameof(ModifierCombinations))]
+        public async Task TestMethodOrderIncorrectReportsAsync(string modifier1, string modifier2)
+        {
+            string placeholder1 = FormatModifier(modifier1);
+            string placeholder2 = FormatModifier(modifier2);
+
+            string testCode =
+$@"class Foo
+{{ 
+    {placeholder1}void Method1()
+    {{
+    }}
+
+    {placeholder2}void Method2()
+    {{
+    }}
+}}";
+
+            var expected = this.CSharpDiagnostic().WithLocation(3, 5).WithArguments(GetModifierName(modifier1), modifier2, "methods");
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Asserts that incorrect ordering for delegates is reported correctly.
+        /// </summary>
+        /// <param name="modifier1">The access modifier for the first delegate.</param>
+        /// <param name="modifier2">The access modifier for the second delegate.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Theory]
+        [MemberData(nameof(ModifierCombinations))]
+        public async Task TestDelegateOrderIncorrectReportsAsync(string modifier1, string modifier2)
+        {
+            string placeholder1 = FormatModifier(modifier1);
+            string placeholder2 = FormatModifier(modifier2);
+
+            string testCode =
+$@"class Foo
+{{ 
+    {placeholder1}delegate void DoSomething1();
+    {placeholder2}delegate void DoSomething2();
+}}";
+
+            var expected = this.CSharpDiagnostic().WithLocation(3, 5).WithArguments(GetModifierName(modifier1), modifier2, "delegates");
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Asserts that incorrect ordering for events is reported correctly.
+        /// </summary>
+        /// <param name="modifier1">The access modifier for the first event.</param>
+        /// <param name="modifier2">The access modifier for the second event.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [Theory]
+        [MemberData(nameof(ModifierCombinations))]
+        public async Task TeskEventOrderIncorrectReportsAsync(string modifier1, string modifier2)
+        {
+            string placeholder1 = FormatModifier(modifier1);
+            string placeholder2 = FormatModifier(modifier2);
+
+            string testCode =
+$@"class Foo
+{{ 
+    {placeholder1}event System.EventHandler<System.EventArgs> MyEvent1;
+    {placeholder2}event System.EventHandler<System.EventArgs> MyEvent2;
+}}";
+
+            var expected = this.CSharpDiagnostic().WithLocation(3, 5).WithArguments(GetModifierName(modifier1), modifier2, "events");
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Performs formatting for the specified modifier prior to insertion into the code template.
+        /// </summary>
+        /// <param name="modifier">The access modifier.</param>
+        /// <returns>A string ready to insert into the code template placeholder.</returns>
+        private static string FormatModifier(string modifier)
+        {
+            return modifier == null ? string.Empty : modifier + " ";
+        }
+
+        private static string GetModifierName(string modifier)
+        {
+            return modifier ?? "private";
+        }
+
+        /// <summary>
+        /// Gets the C# analyzers being tested
+        /// </summary>
+        /// <returns>
+        /// New instances of all the C# analyzers being tested.
+        /// </returns>
+        protected override IEnumerable<DiagnosticAnalyzer> GetCSharpDiagnosticAnalyzers()
+        {
+            yield return new SA1202ElementsMustBeOrderedByAccess();
+        }
+    }
+}
--- StyleCop.Analyzers/StyleCop.Analyzers/OrderingRules/SA1202ElementsMustBeOrderedByAccess.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers/OrderingRules/SA1202ElementsMustBeOrderedByAccess.cs
@@ -1,7 +1,14 @@
 ﻿namespace StyleCop.Analyzers.OrderingRules
 {
+    using System;
+    using System.Collections.Generic;
     using System.Collections.Immutable;
+    using System.Linq;
+    using System.Text;
+
     using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
     using Microsoft.CodeAnalysis.Diagnostics;
 
     /// <summary>
@@ -35,7 +42,7 @@
         /// </summary>
         public const string DiagnosticId = "SA1202";
         private const string Title = "Elements must be ordered by access";
-        private const string MessageFormat = "TODO: Message format";
+        private const string MessageFormat = "All {0} {2} must be placed after all {1} {2}";
         private const string Description = "An element within a C# code file is out of order within regard to access level, in relation to other elements in the code.";
         private const string HelpLink = "http://www.stylecop.com/docs/SA1202.html";
 
@@ -58,6 +65,190 @@
         public override void Initialize(AnalysisContext context)
         {
             // TODO: Implement analysis
+            context.RegisterSyntaxNodeActionHonorExclusions(this.HandleType, SyntaxKind.ClassDeclaration, SyntaxKind.StructDeclaration);
+        }
+
+        private void HandleType(SyntaxNodeAnalysisContext context)
+        {
+            this.HandleMembersOfType<FieldDeclarationSyntax>(context, "fields");
+            this.HandleMembersOfType<MethodDeclarationSyntax>(context, "methods");
+            this.HandleMembersOfType<DelegateDeclarationSyntax>(context, "delegates");
+        }
+
+        private void HandleMembersOfType<T>(SyntaxNodeAnalysisContext context, string memberType) where T : MemberDeclarationSyntax
+        {
+            var typeDeclaration = context.Node as TypeDeclarationSyntax;
+            if (typeDeclaration == null)
+            {
+                return;
+            }
+
+            var data = typeDeclaration.Members.OfType<T>().Select(this.GetModifierData).ToList();
+            var accessModifiers = data.Select(x => x.Priority).Distinct().ToList();
+            for (int i = 0; i < data.Count - 1; i++)
+            {
+                var thisData = data[i];
+                var nextData = data[i + 1];
+                if (nextData.Priority < thisData.Priority)
+                {
+                    var location = thisData.Modifers.Any()
+                        ? thisData.Modifers.First().GetLocation()
+                        : thisData.Node.GetLocation();
+
+                    var diagnostic = Diagnostic.Create(
+                        Descriptor,
+                        location,
+                        this.GetModifierText(thisData.Priority),
+                        this.GetModifierText(this.GetNextLowestPriorityFromPresentTokens(thisData, accessModifiers)),
+                        memberType);
+
+                    context.ReportDiagnostic(diagnostic);
+                }
+            }
+        }
+
+        private struct DeclarationModifierData
+        {
+            public ModifierPriority Priority { get; set; }
+
+            public SyntaxTokenList Modifers { get; set; }
+
+            public SyntaxNode Node { get; set; }
+        }
+
+        private DeclarationModifierData GetModifierData(SyntaxNode node)
+        {
+            var modifiers = GetModifiersFromDeclaration(node);
+            return new DeclarationModifierData
+            {
+                Node = node,
+                Modifers = modifiers,
+                Priority = this.GetAccessModifierOrderPriority(modifiers)
+            };
+        }
+
+        private string GetModifierText(ModifierPriority priority)
+        {
+            switch (priority)
+            {
+                case ModifierPriority.Public:
+                    return "public";
+                case ModifierPriority.Internal:
+                    return "internal";
+                case ModifierPriority.ProtectedInternal:
+                    return "protected internal";
+                case ModifierPriority.Protected:
+                    return "protected";
+                default:
+                    return "private";
+            }
+        }
+
+        private ModifierPriority GetNextLowestPriorityFromPresentTokens(DeclarationModifierData current, List<ModifierPriority> data)
+        {
+            ModifierPriority next = ModifierPriority.Public;
+            for (int index = 0; index < data.Count; index++)
+            {
+                ModifierPriority priority = data[index];
+                if (priority > next && priority < current.Priority)
+                {
+                    next = priority;
+                }
+            }
+
+            return next;
+        }
+
+        /// <summary>
+        /// Defines the access modifiers in priority order.
+        /// </summary>
+        private enum ModifierPriority
+        {
+            Public,
+
+            Internal,
+
+            ProtectedInternal,
+
+            Protected,
+
+            Private,
+        }
+
+        /// <summary>
+        /// Given a token list containing access modifier tokens, returns a <see cref="ModifierPriority"/> that
+        /// describes an order for the access modifier.
+        /// </summary>
+        /// <param name="modifierTokens">A token list containing the access modifier tokens.</param>
+        /// <returns>The priority order.</returns>
+        private ModifierPriority GetAccessModifierOrderPriority(SyntaxTokenList modifierTokens)
+        {
+            if (modifierTokens.Count == 0)
+            {
+                return ModifierPriority.Private;
+            }
+
+            var firstKind = modifierTokens.First().Kind();
+            if (modifierTokens.Count == 2)
+            {
+                var secondKind = modifierTokens[1].Kind();
+                if (firstKind == SyntaxKind.ProtectedKeyword && secondKind == SyntaxKind.InternalKeyword)
+                {
+                    return ModifierPriority.ProtectedInternal;
+                }
+            }
+
+            switch (firstKind)
+            {
+                case SyntaxKind.PublicKeyword:
+                    return ModifierPriority.Public;
+                case SyntaxKind.InternalKeyword:
+                    return ModifierPriority.Internal;
+                case SyntaxKind.ProtectedKeyword:
+                    return ModifierPriority.Protected;
+                case SyntaxKind.PrivateKeyword:
+                    return ModifierPriority.Private;
+            }
+
+            return ModifierPriority.Private;
+        }
+
+        // TODO: Copied from SA1206 - extract into standalone helper.
+        private static SyntaxTokenList GetModifiersFromDeclaration(SyntaxNode node)
+        {
+            SyntaxTokenList result = default(SyntaxTokenList);
+
+            switch (node.Kind())
+            {
+                case SyntaxKind.ClassDeclaration:
+                case SyntaxKind.StructDeclaration:
+                case SyntaxKind.InterfaceDeclaration:
+                    result = ((BaseTypeDeclarationSyntax)node).Modifiers;
+                    break;
+                case SyntaxKind.EnumDeclaration:
+                    result = ((EnumDeclarationSyntax)node).Modifiers;
+                    break;
+                case SyntaxKind.DelegateDeclaration:
+                    result = ((DelegateDeclarationSyntax)node).Modifiers;
+                    break;
+                case SyntaxKind.FieldDeclaration:
+                case SyntaxKind.EventFieldDeclaration:
+                    result = ((BaseFieldDeclarationSyntax)node).Modifiers;
+                    break;
+                case SyntaxKind.PropertyDeclaration:
+                case SyntaxKind.EventDeclaration:
+                case SyntaxKind.IndexerDeclaration:
+                    result = ((BasePropertyDeclarationSyntax)node).Modifiers;
+                    break;
+                case SyntaxKind.MethodDeclaration:
+                case SyntaxKind.ConstructorDeclaration:
+                case SyntaxKind.OperatorDeclaration:
+                case SyntaxKind.ConversionOperatorDeclaration:
+                    result = ((BaseMethodDeclarationSyntax)node).Modifiers;
+                    break;
+            }
+
+            return result;
         }
     }
 }
